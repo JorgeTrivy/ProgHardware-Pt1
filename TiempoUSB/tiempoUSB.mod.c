dc0a99137f3d34de54a42361e1d5777bec78b6fc
JorgeTrivy <160622551+JorgeTrivy@users.noreply.github.com>
2024-02-20 16:44:58 +0100

    Add files via upload

---
+++ TiempoUSB/tiempoUSB.mod.c
@@ -0,0 +1,45 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/elfnote-lto.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+BUILD_LTO_INFO;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x30ff7695, "module_layout" },
+	{ 0x458f49d8, "usb_deregister" },
+	{ 0xb0a1b0c0, "usb_register_driver" },
+	{ 0x4f00afd3, "kmem_cache_alloc_trace" },
+	{ 0xac1c4313, "kmalloc_caches" },
+	{ 0x37a0cba, "kfree" },
+	{ 0x92997ed8, "_printk" },
+	{ 0xc4f0da12, "ktime_get_with_offset" },
+	{ 0x5b8239ca, "__x86_return_thunk" },
+	{ 0xbdfb6dbb, "__fentry__" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "D63F755E2B2111465A1ED9A");
